9141f9cd8556646621ab8317ea6bc236d3ab5f41
Tony Novak <tnovak@cygnuseducation.com>
2026-06-01 10:27:30 -0400

    Support downloading large files that get lazily loaded
    For large PDFs, the image blobs aren't all immediately loaded; rather,
    they're lazily loaded once the placeholder page is scrolled into view.
    To handle this, we find all the page containers, and for each one, add
    the image to the PDF if an image is already present, or scroll to the
    container, wait for the image to appear, and only then continue
    processing it.
    
    Additionally, use the natural size of the image (rather than the
    rendered size) so that the PDF contains full-size images. Also use the
    natural image size as the dimensions of each page in the PDF.

--- view-only-file-download-script.js
+++ view-only-file-download-script.js
@@ -1,38 +1,101 @@
-let trustedURL;
-if (window.trustedTypes && trustedTypes.createPolicy) {
-    const policy = trustedTypes.createPolicy('myPolicy', {
-        createScriptURL: (input) => {
-            return input;
-        }
-    });
-    trustedURL = policy.createScriptURL('https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js');
-} else {
-    trustedURL = 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js';
-}
+(function() {
+  const IMG_SELECTOR = 'img[src^="blob:"]';
 
-// Load the jsPDF library using the trusted URL.
-let jspdf = document.createElement("script");
-jspdf.onload = function () {
-    // Generate a PDF from images with "blob:" sources.
-    let pdf = new jsPDF();
-    let elements = document.getElementsByTagName("img");
-    for (let i = 0; i < elements.length; i++) {
-        let img = elements[i];
-        if (!/^blob:/.test(img.src)) {
-            continue;
-        }
-        let canvasElement = document.createElement('canvas');
-        let con = canvasElement.getContext("2d");
-        canvasElement.width = img.width;
-        canvasElement.height = img.height;
-        con.drawImage(img, 0, 0, img.width, img.height);
-        let imgData = canvasElement.toDataURL("image/jpeg", 1.0);
-        pdf.addImage(imgData, 'JPEG', 0, 0);
-        if (i !== elements.length - 1) {
-            pdf.addPage();
-        }
+  const getPages = function() {
+    const images = [...document.querySelectorAll(IMG_SELECTOR)];
+    if(images.length === 0) {
+      throw new Error("No images found");
     }
-    pdf.save("download.pdf");
-};
-jspdf.src = trustedURL;
-document.body.appendChild(jspdf);
+    const firstPage = images[0].parentElement;
+    const className = firstPage.className;
+    if(!className || className.match(/\s/)) {
+      throw new Error("Couldn't identify CSS class name of page placeholders");
+    }
+
+    return [...firstPage.parentElement.querySelectorAll(`:scope > .${CSS.escape(className)}`)];
+  }
+
+  const buildPDF = async function() {
+    let pdf;
+
+    // Generate a PDF from images with "blob:" sources.
+    for(const page of getPages()) {
+      const img = await getImage(page);
+      const width = img.naturalWidth;
+      const height = img.naturalHeight;
+
+      if(!pdf) {
+        pdf = new jsPDF({ unit: 'px', format: [width, height] });
+      }
+      else {
+        pdf.addPage([width, height]);
+      }
+
+      const canvasElement = document.createElement('canvas');
+      const con = canvasElement.getContext("2d");
+      canvasElement.width = width;
+      canvasElement.height = height;
+      con.drawImage(img, 0, 0, width, height);
+      const imgData = canvasElement.toDataURL("image/jpeg", 1.0);
+      pdf.addImage(imgData, 'JPEG', 0, 0, width, height);
+    }
+
+    return pdf;
+  };
+
+  // Get the image on the specified page if it's already loaded, or wait for it to load
+  const getImage = async function(page) {
+    const img = page.querySelector(IMG_SELECTOR) || await loadImage(page);
+    if(img.complete) {
+      return img;
+    }
+    return new Promise((resolve, reject) => {
+      img.onload = () => resolve(img);
+      img.onerror = (e) => reject(e);
+    });
+  };
+
+  // Scroll to the page and wait for the image to load
+  const loadImage = function(page) {
+    return new Promise((resolve, _reject) => {
+      const mutationCallback = (_, observer) => {
+        const img = page.querySelector(IMG_SELECTOR);
+        if(img) {
+          observer.disconnect();
+          resolve(img);
+        }
+      };
+      new MutationObserver(mutationCallback).observe(page, { childList: true, attributes: true, subtree: true });
+      page.scrollIntoView();
+    });
+  }
+
+  const downloadPDF = function() {
+    buildPDF()
+      .then(pdf => {
+        pdf.save("download.pdf");
+      })
+      .catch((e) => {
+        console.log(e);
+        alert(e);
+      });
+  }
+
+  let trustedURL;
+  if (window.trustedTypes && trustedTypes.createPolicy) {
+      const policy = trustedTypes.createPolicy('myPolicy', {
+          createScriptURL: (input) => {
+              return input;
+          }
+      });
+      trustedURL = policy.createScriptURL('https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js');
+  } else {
+      trustedURL = 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js';
+  }
+
+  // Load the jsPDF library using the trusted URL.
+  const jspdf = document.createElement("script");
+  jspdf.onload = downloadPDF;
+  jspdf.src = trustedURL;
+  document.body.appendChild(jspdf);
+})();
